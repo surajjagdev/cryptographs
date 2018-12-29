4847ffd3763f850073f63c6ebf8be5f15d1bbb4f
Suraj Jagdev <suraj_57@hotmail.com>
2018-12-29 17:28:32 -0500

    added in navbar component using css-grid systems and imported into app.js file

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from '../Nav/Nav.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <header>Welcome to CrytoGraphs</header>
       </div>
     );
---
+++ src/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Nav.css';
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <div>CryptoDash</div>
+      <div />
+      <div>DashBoard</div>
+      <div>Settings</div>
+    </div>
+  );
+};
+export default NavBar;
