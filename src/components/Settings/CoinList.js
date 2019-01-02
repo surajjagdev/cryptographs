dcd3daf433b53892bfa4fd3abfe4e11ca8753f5e
Suraj Jagdev <suraj_57@hotmail.com>
2019-01-02 17:02:22 -0500

    changed background of coinList to being slightly lighter in color and text-aligning as well as margining

--- src/components/Settings/CoinList.js
+++ src/components/Settings/CoinList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { AppContext } from '../App/AppProvider';
 import Grid from './Grid.js';
+import './CoinList.css';
 export default function() {
   return (
     <AppContext.Consumer>
       {({ coinList }) => (
         <Grid>
           {Object.keys(coinList).map(coinKey => (
-            <div>{coinKey}</div>
+            <div className="coinListGrid">{coinKey}</div>
           ))}
         </Grid>
       )}
