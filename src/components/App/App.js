6d9ee938725293a1b73582d97c819faa4a4957e4
Suraj Jagdev <suraj_57@hotmail.com>
2019-01-02 15:40:40 -0500

    added in Loading screen till coins load in

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import NavBar from '../Nav/Nav.js';
 import Settings from '../Settings/Settings.js';
 import AppProvider from './AppProvider.js';
+import Content from '../SharedPages/Content';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppProvider>
           <NavBar />
-          <Settings />
+          <Content>
+            <Settings />
+          </Content>
         </AppProvider>
       </div>
     );
